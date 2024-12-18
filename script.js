40d3efaf66eee1de54458ca9554f59835c10c2f7
Abhijit Paul <abhijitpaul0212@gmail.com>
2024-12-18 22:30:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Image Slider
+document.querySelectorAll('.slider-container').forEach(container => {
+    const slider = container.querySelector('.product-slider');
+    const leftBtn = container.querySelector('.slider-btn.left');
+    const rightBtn = container.querySelector('.slider-btn.right');
+
+    let scrollAmount = 0;
+
+    leftBtn.addEventListener('click', () => {
+        scrollAmount -= 100;
+        slider.scrollTo({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+
+    rightBtn.addEventListener('click', () => {
+        scrollAmount += 100;
+        slider.scrollTo({
+            left: scrollAmount,
+            behavior: 'smooth'
+        });
+    });
+});
+
 // Modal Popup
 const images = document.querySelectorAll('.slider-image');
 const modal = document.getElementById('image-modal');
