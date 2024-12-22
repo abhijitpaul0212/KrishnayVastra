990a957a13af3583ee50d0a73be7d10ec405d0bf
Abhijit Paul <abhijitpaul0212@gmail.com>
2024-12-22 15:53:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// // Modal Popup
-// const images = document.querySelectorAll('.slider-image');
-// const modal = document.getElementById('image-modal');
-// const modalImage = document.getElementById('popup-image');
-// const closeModal = document.querySelector('.close');
+// Modal Popup
+const images = document.querySelectorAll('.slider-image');
+const modal = document.getElementById('image-modal');
+const modalImage = document.getElementById('popup-image');
+const closeModal = document.querySelector('.close');
 
-// images.forEach(image => {
-//     image.addEventListener('click', () => {
-//         modal.style.display = 'flex';
-//         modalImage.src = image.src;
-//     });
-// });
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        modal.style.display = 'flex';
+        modalImage.src = image.src;
+    });
+});
 
-// closeModal.addEventListener('click', () => {
-//     modal.style.display = 'none';
-// });
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
 
-// window.addEventListener('click', (e) => {
-//     if (e.target === modal) {
-//         modal.style.display = 'none';
-//     }
-// });
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = 'none';
+    }
+});
 
 
 // Cart Logic
